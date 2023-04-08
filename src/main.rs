d58829d855994cf866ada43ad73ccac2d2e8f195
AndrewB3301 <Andrii.Borziak.330@gmail.com>
2023-04-08 00:30:02 -0700

    Enabled watch for changes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use voxel_volume_material::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            // Tell the asset server to watch for asset changes on disk:
+            watch_for_changes: true,
+            ..default()
+        }))
         .add_plugin(MaterialPlugin::<VoxelVolumeMaterial>::default())
         .add_startup_system(setup)
         .add_system(rotate_camera)
